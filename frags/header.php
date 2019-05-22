e516db7e5d508c6b6b5ce39a94a3a1d19c8a2730
Andrew <andrew.schleumer@gmail.com>
2019-05-22 12:16:16 -0300

    Index

--- frags/header.php
+++ frags/header.php
@@ -1,8 +1,48 @@
-<?php
+<style>
+    html{ height:100%; }
+    body{ min-height:100%; padding:0; margin:0; position:relative; }
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+    body::after{ content:''; display:block; height:100px; }
 
+    .footer {
+        position: absolute;
+        bottom: 0;
+        width: 100%;
+        height: 50px;
+    }
+
+    body {
+        width: 100%;
+        height: 400px;
+        background-size: 100% 100%;
+    }
+
+    .transparent{
+        background:rgba(255,255,255,0.5);
+    }
+
+</style>
+
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <a class="navbar-brand" href="#">
+        <img src="imgs/doria.jpg" width="30" height="30" alt="">
+    </a>
+    <span class="navbar-brand mb-0 h1">Loja Bizarra</span>
+
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto">
+
+            <li class="nav-item">
+                <a class="nav-link active" href="#">Perfil</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link active" href="#">Produtos</a>
+            </li>
+
+        </ul>
+        <form class="form-inline my-2 my-lg-0">
+            <input class="form-control mr-sm-2" type="search" placeholder="Pesquisar roduto" aria-label="Pesquisar produto">
+            <button class="btn btn-light my-2 my-sm-0" type="submit">Pesquisar</button>
+        </form>
+    </div>
+</nav>
--- index.php
+++ index.php
@@ -2,10 +2,34 @@
 <html>
     <head>
         <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
         <title>Loja Bizarra</title>
     </head>
-    <body>
-        <h1>Loja Bizarra</h1>
+    <body background="imgs/background.jpg">
+
+        <?php include 'frags/header.php'; ?>
+
+        <div class="container-fluid">
+
+            <div class="row"> 
+
+                
+                    <div class="container"> 
+                        <div class="col transparent" style="background-color: whitesmoke">
+                            <div class="row">
+                                <div class="col-8 ">
+                                    
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                
+            </div>
+
+        </div>
+
+
+        <?php include 'frags/footer.php'; ?>
     </body>
 </html>
